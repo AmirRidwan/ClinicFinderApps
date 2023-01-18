858afb5ea0a0fb905b859cd48bba750cc66a863e
AmirRidwan <amirridwan59@gmail.com>
2023-01-19 02:08:28 +0800

    First commit

--- app/src/main/java/com/example/clinicfinderapps/MainActivity.java
+++ app/src/main/java/com/example/clinicfinderapps/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.clinicfinderapps;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    CardView cardView1, cardView2, cardView3, cardView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        cardView1 = findViewById(R.id.current_location);
+        cardView2 = findViewById(R.id.clinic_near_me);
+        cardView3 = findViewById(R.id.about_us);
+        cardView4 = findViewById(R.id.logout);
+
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), CurrentLocation.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), ClinicNearMe.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), AboutUs.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        cardView4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), Login.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
